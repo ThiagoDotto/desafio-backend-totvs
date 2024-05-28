df9859b1db6e15301fbe2402d9289e1fa1f2d8dd
thiago <tdotto@gmail.com>
2024-05-28 11:58:27 -0300

    Primeiro commit. Configuração do gitconfig

---
+++ src/main/java/com/totvs/desafiobackendtotvs/DesafioBackendTotvsApplication.java
@@ -0,0 +1,13 @@
+package com.totvs.desafiobackendtotvs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackendTotvsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackendTotvsApplication.class, args);
+	}
+
+}
